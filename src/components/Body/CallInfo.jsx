d3770b33fd18c6fdb5ed9d78f7336fb910d4b590
javascriptsucks <evix1988@live.cn>
2023-08-04 17:06:52 -0600

    use model to return info when click callback button

--- src/components/Body/CallInfo.jsx
+++ src/components/Body/CallInfo.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import { useSelector } from "react-redux";
 import { IconButton } from "@mui/material";
 import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
@@ -9,6 +15,16 @@ import CallIconGenerator from "../../utils/callIconGenerator.jsx";
 
 function CallInfo({ activity }) {
 	const { call_type, created_at, from, to } = activity;
+
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 
 	const { viewArchived } = useSelector((state) => state.activities);
 
@@ -25,9 +41,31 @@ function CallInfo({ activity }) {
 			<IconButton
 				className={styles.callInfoIconButton}
 				size='small'
-				style={{ color: call_type === "missed" ? "#F31559" : "#CECE5A" }}>
+				style={{ color: call_type === "missed" ? "#F31559" : "#CECE5A" }}
+				onClick={handleClickOpen}>
 				<CallIconGenerator callType={call_type} />
 			</IconButton>
+
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby='alert-dialog-title'
+				aria-describedby='alert-dialog-description'>
+				<DialogTitle id='alert-dialog-title'>Call details</DialogTitle>
+				<DialogContent>
+					<DialogContentText id='alert-dialog-description'>
+						{`Do you want to call this ${call_type} from ${from}?`}
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Close</Button>
+					<Button
+						onClick={handleClose}
+						autoFocus>
+						Call
+					</Button>
+				</DialogActions>
+			</Dialog>
 
 			<div className={styles.callInfo}>
 				<div className={styles.callInfoFrom}>
